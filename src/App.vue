48e872eb593dad8ed684628c3294b58eaa9c0faf
GRLuis1565 <luisggr2004@gmail.com>
2025-06-10 14:21:20 -0600

    Primer comit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+        <h1>Reporte de PowerBI</h1>
+        <p>Visualizacion interactiva de datos generada en Power BI</p>
+        <div class="iframe-container">
+            <iframe title="Visualisacion_datamartNortwing2" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNWE0YTIzZjUtMjNlYS00NGRlLTkyMTktMTFmZTQ0MmEwNzNmIiwidCI6Ijg1NTliOGI4LTBiMTEtNDg2ZS04YmQzLTdiNWRhM2NmMzIxZSJ9" frameborder="0" allowFullScreen="true"></iframe>
+        </div>
+    </div>
+</template>
+
